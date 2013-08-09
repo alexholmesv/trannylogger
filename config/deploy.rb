ef207a439b11665593b28ff598d4b9e1322d8adf
Ignacio Verdejo <iverdejo@acid.cl>
2013-08-09 15:36:32 -0400

    Add fix in assets task and descriptions

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,18 +33,18 @@ role :db, "ubuntuservervbox"
 # end
 
 namespace :deploy do
-  desc "Create and set permittions for capistrano directory structure"
+  desc "Create and set permittions for capistrano directory structure."
 	task :setup do
     run "mkdir -p #{deploy_to} #{deploy_to}/releases #{deploy_to}/shared #{deploy_to}/shared/system #{deploy_to}/shared/log #{deploy_to}/shared/pids"
     run "chmod g+w #{deploy_to} #{deploy_to}/releases #{deploy_to}/shared #{deploy_to}/shared/system #{deploy_to}/shared/log #{deploy_to}/shared/pids"
 	end
 
-  desc "Clone project in a new release path and install gems in Gemfile"
+  desc "Clone project in a new release path and install gems in Gemfile."
   task :update_release do
     run "git clone #{repository} -b #{branch} #{deploy_to}/releases/#{release_name} && cd #{deploy_to}/releases/#{release_name} && bundle install --without development test"
   end
 
-  desc "Create database yaml in shared path"
+  desc "Create database yaml in shared path."
   task :db_configure do
     db_config = <<-EOF
 #{rails_env}:
@@ -53,13 +53,13 @@ namespace :deploy do
   pool:     25
   username: #{database_username}
   password: #{database_password}
-    EOF
+EOF
 
     run "rm -Rf #{shared_path}/config && mkdir -p #{shared_path}/config"
     put db_config, "#{shared_path}/config/database.yml"
   end
 
-  desc "Create thin configuration yaml in shared path"
+  desc "Create thin configuration yaml in shared path."
   task :thin_configure do
     thin_config = <<-EOF
 ---
@@ -77,40 +77,40 @@ require:              []
 wait:                 #{server_wait}
 servers:              #{server_threads}
 daemonize:            true
-    EOF
+EOF
 
     run "rm -Rf #{shared_path}/thin && mkdir -p #{shared_path}/thin"
     put thin_config, "#{shared_path}/thin/server.yml"
   end
 
-  desc "Make symlink for database yaml"
+  desc "Make symlink for database yaml."
   task :db_symlink do
     run "ln -snf #{shared_path}/config/database.yml #{current_path}/config/database.yml"
   end
 
-  desc "Configuring database for project"
+  desc "Configuring database for project."
   task :db_setup do
     run "cd #{current_path} && bundle exec rake db:setup RAILS_ENV=#{rails_env}"
   end
 
-  desc "Start Thin server"
+  desc "Start Thin server."
   task :start do
     run "cd #{current_path} && bundle exec thin start -C #{shared_path}/thin/server.yml"
   end
 
-  desc "Stop Thin server"
+  desc "Stop Thin server."
   task :stop do
     run "cd #{current_path} && bundle exec thin stop -C #{shared_path}/thin/server.yml"
   end
 
-  desc "Restart Thin server"
+  desc "Restart Thin server."
   task :restart do
     run "cd #{current_path} && bundle exec thin restart -C #{shared_path}/thin/server.yml"
   end
 
-  desc "Run assets:precompile rake task for the deployed application"
+  desc "Run assets:precompile rake task for the deployed application."
   task :assets do
-    run "cd #{current_path} && bundle exec rake assets:precompile RAILS_ENV=#{rails_env}"
+    run "cd #{current_path} && bundle exec rake assets:precompile RAILS_ENV=#{rails_env} RAILS_GROUPS=assets"
   end
 
   desc "First time deploy"
