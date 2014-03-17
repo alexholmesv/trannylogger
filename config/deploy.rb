fcd95dfa5e1a4ec5ead1baf8961001f8eacbb1a5
Ignacio Verdejo <iverdejo@acid.cl>
2014-03-17 22:09:50 +0000

    Fix indentation

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,10 +19,10 @@ set :ssh_options, { forward_agent: true}
 
 namespace :deploy do
   desc "Create and set permittions for capistrano directory structure."
-	task :setup, roles: :app do
+  task :setup, roles: :app do
     run "mkdir -p #{deploy_to} #{deploy_to}/releases #{deploy_to}/shared #{deploy_to}/shared/system #{deploy_to}/shared/log #{deploy_to}/shared/pids"
     run "chmod g+w #{deploy_to} #{deploy_to}/releases #{deploy_to}/shared #{deploy_to}/shared/system #{deploy_to}/shared/log #{deploy_to}/shared/pids"
-	end
+  end
 
   desc "Clone project in a new release path and install gems in Gemfile."
   task :update_release, roles: :app do
