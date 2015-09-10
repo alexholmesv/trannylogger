b11ec4c33970f362a4e2a6b614aea793c826b200
Ignacio Verdejo Lagos <iverdejo@acid.cl>
2015-09-10 10:37:49 -0300

    Add sidekiq process id path for each environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,7 @@ server 'app_server:22', :app, :db, primary: true
 set :rails_env, "staging"
 set :branch, "master"
 set :deploy_to, "/home/deploy/#{rails_env}.#{app_stage}"
+set :sidekiq_pid, File.join(shared_path, 'pids', 'sidekiq.pid')
 
 #Database settings
 set :database_adapter, "postgresql"
