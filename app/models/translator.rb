11bc4cd3ff66b5bf92dcba339bf9cceab94353b8
Alexander Holmes <alex@acid.cl>
2016-04-05 10:38:32 -0300

    add styling

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -1,2 +1,12 @@
 class Translator < ActiveRecord::Base
+	has_many :projects
+
+	def last_project_name
+		last_project ? last_project.name : nil
+	end
+
+	def last_project
+		projects.last
+	end
+
 end
