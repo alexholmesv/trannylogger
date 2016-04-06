e0e27a085f7f689bd694116261f278ee228c45b6
Alexander Holmes <alex@acid.cl>
2016-04-06 12:30:26 -0300

    fix devise views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   before_action :set_clients, only: [:new, :create, :update, :edit]
   before_action :set_translators, only: [:new, :create, :update, :edit]
+  before_action :authenticate_user!
   
   # GET /projects
   # GET /projects.json
--- app/controllers/translators_controller.rb
+++ app/controllers/translators_controller.rb
@@ -1,5 +1,6 @@
 class TranslatorsController < ApplicationController
   before_action :set_translator, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /translators
   # GET /translators.json
